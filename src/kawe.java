4a9b0447ae5da351618bc1d792ab3893b97b01af
Kaue Jose Santos da Silva <kaue.santos@labs.dcx>
2023-06-01 11:33:04 -0300

    Função soma

---
+++ src/kawe.java
@@ -0,0 +1,11 @@
+public class kawe {
+
+    private int idade;
+    private int numero;
+    private int numeroResidencia;
+
+    public void soma() {
+        numero = idade + numeroResidencia;
+    }
+
+}
